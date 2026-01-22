6654f1fc6f26871c8931ebb77c7dd14ae72461b1
hmanner0930 <hinckleymanner@gmail.com>
2026-01-21 17:27:05 -0700

    Changes second add pieces in reset board

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -45,13 +45,46 @@ public class ChessBoard {
     public void resetBoard() {
         for(int r=0; r<8; r++){
             for (int c=0; c<8;c++){
-                board[c][r] = null;
+                board[r][c] = null;
             }
         }
+        ChessGame.TeamColor WHITE = ChessGame.TeamColor.WHITE;
+        ChessGame.TeamColor BLACK = ChessGame.TeamColor.BLACK;
+        addPiece(new ChessPosition(1,1), new ChessPiece(WHITE,ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1,2), new ChessPiece(WHITE,ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1,3), new ChessPiece(WHITE,ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1,4), new ChessPiece(WHITE,ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(1,5), new ChessPiece(WHITE,ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(1,6), new ChessPiece(WHITE,ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1,7), new ChessPiece(WHITE,ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1,8), new ChessPiece(WHITE,ChessPiece.PieceType.ROOK));
+        // for pawnss
+        for(int c = 1; c<=8; c++){
+            addPiece(new ChessPosition(2,c), new ChessPiece(WHITE, ChessPiece.PieceType.PAWN));
+        }
 
+        addPiece(new ChessPosition(8,1), new ChessPiece(BLACK,ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8,2), new ChessPiece(BLACK,ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8,3), new ChessPiece(BLACK,ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8,4), new ChessPiece(BLACK,ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8,5), new ChessPiece(BLACK,ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8,6), new ChessPiece(BLACK,ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8,7), new ChessPiece(BLACK,ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8,8), new ChessPiece(BLACK,ChessPiece.PieceType.ROOK));
+        // for pawns
+        for(int c = 1; c<=8; c++){
+            addPiece(new ChessPosition(7,c), new ChessPiece(BLACK, ChessPiece.PieceType.PAWN));
+        }
     }
     @Override
     public int hashCode() {
         return Arrays.deepHashCode(board);
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessBoard that = (ChessBoard) o;
+        return Arrays.deepEquals(this.board, that.board);
     }
 }
