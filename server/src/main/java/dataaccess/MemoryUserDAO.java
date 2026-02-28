1ee3a11595c1690db9db501174787aaf2aed3f74
hmanner0930 <hinckleymanner@gmail.com>
2026-02-27 17:14:20 -0700

    Trying to get started

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,23 @@
+package dataaccess;
+
+import model.UserData;
+import java.util.HashMap;
+
+public class MemoryUserDAO implements UserDAO {
+    private final HashMap<String, UserData> users = new HashMap<>();
+
+    @Override
+    public void insertUser(UserData u) {
+        users.put(u.username(), u);
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    @Override
+    public void clear() {
+        users.clear();
+    }
+}
