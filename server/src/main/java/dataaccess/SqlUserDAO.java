071e0a2f8c710e7d73254bf257a840cb0ba38f59
hmanner0930 <hinckleymanner@gmail.com>
2026-03-10 11:17:42 -0600

    Implemented SqlUserDAO.java

---
+++ server/src/main/java/dataaccess/SqlUserDAO.java
@@ -0,0 +1,45 @@
+package dataaccess;
+
+import model.UserData;
+import org.mindrot.jbcrypt.BCrypt;
+import java.sql.*;
+
+public class SqlUserDAO implements UserDAO{
+    public void createUser(UserData user) throws DataAccessException{
+        String hashedPassword = BCrypt.hashpw(user.password(), BCrypt.gensalt());
+        String sql = "Insert INTO user (username, password, email) VALUES (? ? ?)";
+        try(var conn = DatabaseManager.getConnection()){
+            try(var ps = conn.prepareStatement(sql)){
+                ps.setString(1, user.username());
+                ps.setString(2, hashedPassword);
+                ps.setString(3, user.email());
+                ps.executeUpdate();
+            }
+        } catch (SQLException e){
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var ps = conn.prepareStatement("SELECT username, password, email FROM user WHERE username=?");
+            ps.setString(1, username);
+            try (var rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new UserData(rs.getString(1), rs.getString(2), rs.getString(3));
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return null;
+    }
+
+    public void clear() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            conn.prepareStatement("TRUNCATE TABLE user").executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+}
