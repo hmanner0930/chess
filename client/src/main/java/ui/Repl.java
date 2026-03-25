541422f9bd11f2ec703b2429312928af0a1ecffa
hmanner0930 <hinckleymanner@gmail.com>
2026-03-25 17:54:40 -0600

    Changes to REPL

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -12,20 +12,20 @@ public class Repl {
     }
 
     public void run() {
-        System.out.println(WHITE_PAWN + " Welcome to 240 Chess. Type Help to get started. " + WHITE_PAWN);
+        System.out.println(WHITE_PAWN + " Welcome to 240 Chess." + WHITE_PAWN);
 
         Scanner scanner = new Scanner(System.in);
         var result = "";
         while (!result.equals("quit")) {
-            String printState = client.getState(); // e.g., "LOGGED_OUT" or "LOGGED_IN"
+            String printState = client.getState();
             System.out.print("\n" + SET_TEXT_COLOR_WHITE + "[" + printState + "] >>> " + SET_TEXT_COLOR_GREEN);
             String line = scanner.nextLine();
 
             try {
                 result = client.eval(line);
                 System.out.print(SET_TEXT_COLOR_BLUE + result);
-            } catch (Throwable e) {
-                System.out.print(SET_TEXT_COLOR_RED + e.getMessage());
+            } catch (Throwable exception) {
+                System.out.print(SET_TEXT_COLOR_RED + exception.getMessage());
             }
         }
         System.out.println();
